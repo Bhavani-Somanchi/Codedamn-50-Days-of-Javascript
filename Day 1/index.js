0c568b8790bdf78f10eb3ad05228c8f8771341f9
Bhavani Somanchi <45579660+Bhavani-Somanchi@users.noreply.github.com>
2021-11-26 16:55:25 +0530

    Update index.js

--- Day 1/index.js
+++ Day 1/index.js
@@ -1,3 +1,4 @@
+
 function randomNumberGeneratorInRange(rangeStart, rangeEnd) {
 	// write your solution here
 
@@ -5,3 +6,4 @@ function randomNumberGeneratorInRange(rangeStart, rangeEnd) {
 }
 
 console.log(`My random number: ${randomNumberGeneratorInRange(5, 100)}`)
+
