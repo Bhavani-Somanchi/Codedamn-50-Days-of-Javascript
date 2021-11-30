1ad3a75b865713088d461139b8e666130aaeb2ba
Bhavani Somanchi <bhavanisomanchi06@gmail.com>
2021-11-30 06:46:48 +0530

    Day 19

---
+++ Day 19/index.js
@@ -0,0 +1,8 @@
+const colorCode = (color) => {
+	// Code here
+
+    var arr = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+	return arr.indexOf(color);
+}
+
+console.log(colorCode('white'));
